8e633c22300ff9cd48df7074c52946ecc8c6e2a2
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2022-10-18 20:51:57 +0200

    Remove service worker.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
